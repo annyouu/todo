db7a5589f8b49e559a8ee2cd052ac4a4f790d3c8
antou <andong041205@gmail.com>
2026-04-26 17:08:15 +0900

    [add] frontend初期設定

---
+++ frontend/frontend/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  frontend
+//
+//  Created by KAKUANROU on 2026/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ frontend/frontend/frontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  frontendApp.swift
+//  frontend
+//
+//  Created by KAKUANROU on 2026/04/26.
+//
+
+import SwiftUI
+
+@main
+struct frontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
